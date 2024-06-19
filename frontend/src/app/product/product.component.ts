1fcd5c551a1596f966aba5c2f1a1994c27eae1b3
Eken4 <juliette.bressy@free.fr>
2024-06-19 21:29:22 +0200

    bouton ajouter panier

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { Product } from '../models/product';
+import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [],
+  providers: [ShoppingCartComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
   inputs: ['data']
 })
 export class ProductComponent {
-public declare data: Product
+  public declare data: Product
+
+  constructor(private cart: ShoppingCartComponent) { }
+
+  addToCart() {
+
+  }
 }
